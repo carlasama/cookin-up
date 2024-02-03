a117d8c46c37a1cfbcfd7a95f887f1529c948be4
carla samaniego <carlavxsamaniego@gmail.com>
2024-02-03 14:57:18 -0400

    Cookin Up

---
+++ src/components/Rodape.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+    <footer class="rodape">
+        <p class="paragrafo rodape-texto">
+            Desenvolvido por Alura | 2023 - Projeto fictício sem fins comerciais.
+        </p>
+    </footer>
+</template>
+
+<style scoped>
+.rodape {
+    background-color: #263A29;
+    color: #fffaf3;
+    padding: 16px;
+    text-align: center;
+}
+</style>
